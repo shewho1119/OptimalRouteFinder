9b292bd9eb3790de2a27b7ebe06aa724e43c9daf
shewho1119 <parkshihoo1119@gmail.com>
2024-05-28 16:46:18 +1200

    Override HashCode and equals() in the country (node)

--- src/main/java/nz/ac/auckland/se281/Country.java
+++ src/main/java/nz/ac/auckland/se281/Country.java
@@ -19,6 +19,32 @@ public class Country {
     this.tax = tax;
   }
 
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    result = prime * result + ((continent == null) ? 0 : continent.hashCode());
+    result = prime * result + tax;
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+    Country other = (Country) obj;
+    if (name == null) {
+      if (other.name != null) return false;
+    } else if (!name.equals(other.name)) return false;
+    if (continent == null) {
+      if (other.continent != null) return false;
+    } else if (!continent.equals(other.continent)) return false;
+    if (tax != other.tax) return false;
+    return true;
+  }
+
   public String getName() {
     return name;
   }
